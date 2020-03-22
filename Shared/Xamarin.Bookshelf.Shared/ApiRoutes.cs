51603ab9e5ed980fd331ee1f7a7f6b2c7a8030e2
Alexandre Costa <alexandresantoscosta@gmail.com>
2020-03-21 21:09:12 -0300

    Added API Routs constantes and started to model client api service

---
+++ Shared/Xamarin.Bookshelf.Shared/ApiRoutes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.Bookshelf.Shared
+{
+    public class ApiRoutes
+    {
+        public const string API_SEARCH_BOOKS = "v1/books";
+        public const string API_GET_BOOK_DETAILS = "v1/books/{id}";
+        public const string API_SEND_BOOK_REVIEW = "v1/reviews";
+        public const string API_REGISTER_BOOK = "v1/bookshelves";
+        public const string API_GET_USER_BOOKS = "v1/bookshelves/{userId}/";
+        public const string API_GET_BOOK_REVIEWS = "v1/reviews/{bookId}/";
+    }
+}
--- Shared/Xamarin.Bookshelf.Shared/Services/IBookService.cs
+++ Shared/Xamarin.Bookshelf.Shared/Services/IBookService.cs
@@ -1,13 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using Refit;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xamarin.Bookshelf.Shared.Models;
+using ABookshelf = Xamarin.Bookshelf.Shared.Models.Bookshelf;
 
 namespace Xamarin.Bookshelf.Shared.Services
 {
     public interface IBookService
     {
+        [Get("")]
         Task<IEnumerable<Book>> SearchBookByTitleAsync(string title);
+        [Get("")]
         Task<IEnumerable<Book>> SearchBookByAuthorAsync(string title);
+        [Get("")]
         Task<Book> GetBookDetails(string bookId);
+        [Get("")]
+        Task<IEnumerable<BookReview>> GetBookReviews(string bookId);
+            [Get("")]
+        Task<IEnumerable<ABookshelf>> GetUserBookShelves(string userId);
+        [Post("")]
+        Task RegisterBook(ABookshelf bookshelf);
+            [Post("")]
+        Task ReviewBook(BookReview review);
     }
 }
