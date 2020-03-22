636a0201ac27014edb36155de463c402379a7af3
Alexandre Costa <alexandresantoscosta@gmail.com>
2020-03-22 08:29:59 -0300

    Created first integration test to fix Refit configuration issues

---
+++ Tests/Xamarin.Bookshelf.Integration.Tests/BooksApiTests.cs
@@ -0,0 +1,24 @@
+using Refit;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Bookshelf.Shared;
+using Xamarin.Bookshelf.Shared.Models;
+using Xamarin.Bookshelf.Shared.Services;
+using Xunit;
+
+namespace Xamarin.Bookshelf.Integration.Tests
+{
+    public class BooksApiTests
+    {
+        [Fact]
+        public async Task SearchBooksByTitle()
+        {
+            IBookService service = RestService.For<IBookService>(ApiRoutes.API_BASE_URL);
+
+            var books = await service.SearchBookByTitleAsync("mochileiro");
+
+            Assert.Equal(10, books.Count());
+        }
+    }
+}
