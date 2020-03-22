a51d2ace795c841cce827715a15ac5733fd9660d
Alexandre Costa <alexandresantoscosta@gmail.com>
2020-03-22 08:32:12 -0300

    Fixes in Refit interface and Test project added to the solution

--- Shared/Xamarin.Bookshelf.Shared/ApiRoutes.cs
+++ Shared/Xamarin.Bookshelf.Shared/ApiRoutes.cs
@@ -6,11 +6,14 @@ namespace Xamarin.Bookshelf.Shared
 {
     public class ApiRoutes
     {
-        public const string API_SEARCH_BOOKS = "v1/books";
-        public const string API_GET_BOOK_DETAILS = "v1/books/{id}";
-        public const string API_SEND_BOOK_REVIEW = "v1/reviews";
-        public const string API_REGISTER_BOOK = "v1/bookshelves";
-        public const string API_GET_USER_BOOKS = "v1/bookshelves/{userId}/";
-        public const string API_GET_BOOK_REVIEWS = "v1/reviews/{bookId}/";
+        public const string API_BASE_URL = "https://xamarin-bookshelf.azurewebsites.net/";
+        public const string API_BOOKS = "v1/books";
+        public const string API_SEARCH_BOOKS_BY_TITLE = API_BOOKS + "?title={title}";
+        public const string API_SEARCH_BOOKS_BY_AUTOR = API_BOOKS + "?author={author}";
+        public const string API_GET_BOOK_DETAILS = API_BOOKS + "/{id}";
+        public const string API_REVIEWS = "v1/reviews";
+        public const string API_BOOKSHELVES = "v1/bookshelves";
+        public const string API_GET_USER_BOOKS = API_BOOKSHELVES + "/{userId}/";
+        public const string API_GET_BOOK_REVIEWS = API_REVIEWS + "/{bookId}/";
     }
 }
--- Shared/Xamarin.Bookshelf.Shared/Services/IBookService.cs
+++ Shared/Xamarin.Bookshelf.Shared/Services/IBookService.cs
@@ -6,21 +6,22 @@ using ABookshelf = Xamarin.Bookshelf.Shared.Models.Bookshelf;
 
 namespace Xamarin.Bookshelf.Shared.Services
 {
+    [Headers("x-functions-key: YlQpARa4UL1VGG2Jem8f4jvuxaBXhP0sSNFdTRED08yvbrOXqN2aXA==")]
     public interface IBookService
     {
-        [Get("")]
+        [Get("/v1/books?title={title}")]
         Task<IEnumerable<Book>> SearchBookByTitleAsync(string title);
-        [Get("")]
-        Task<IEnumerable<Book>> SearchBookByAuthorAsync(string title);
-        [Get("")]
-        Task<Book> GetBookDetails(string bookId);
-        [Get("")]
-        Task<IEnumerable<BookReview>> GetBookReviews(string bookId);
-            [Get("")]
-        Task<IEnumerable<ABookshelf>> GetUserBookShelves(string userId);
-        [Post("")]
-        Task RegisterBook(ABookshelf bookshelf);
-            [Post("")]
-        Task ReviewBook(BookReview review);
+        [Get("/v1/books?author={author}")]
+        Task<IEnumerable<Book>> SearchBookByAuthorAsync(string author);
+        [Get("/v1/books/{id}")]
+        Task<Book> GetBookDetailsAsync(string id);
+        [Get("/v1/reviews/{bookId}")]
+        Task<IEnumerable<BookReview>> GetBookReviewsAsync(string bookId);
+            [Get("/v/1/bookshelves/{userId}")]
+        Task<IEnumerable<ABookshelf>> GetUserBookShelvesAsync(string userId);
+        [Post("/v1/bookshelves")]
+        Task RegisterBookAsync(ABookshelf bookshelf);
+            [Post("/v1/reviews")]
+        Task ReviewBookAsync(BookReview review);
     }
 }
