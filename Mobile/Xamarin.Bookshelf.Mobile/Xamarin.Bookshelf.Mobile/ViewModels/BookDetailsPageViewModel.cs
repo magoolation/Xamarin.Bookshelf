3c7ec34405dd02c56abba28b1320980df197cf51
Alexandre Costa <alexandresantoscosta@gmail.com>
2020-03-22 21:39:34 -0300

    Added the Add to My Library feature

--- Mobile/Xamarin.Bookshelf.Mobile/Xamarin.Bookshelf.Mobile/ViewModels/BookDetailsPageViewModel.cs
+++ Mobile/Xamarin.Bookshelf.Mobile/Xamarin.Bookshelf.Mobile/ViewModels/BookDetailsPageViewModel.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Bookshelf.Shared.Models;
 using Xamarin.Bookshelf.Shared.Services;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using ABookshelf = Xamarin.Bookshelf.Shared.Models.Bookshelf;
 
 namespace Xamarin.Bookshelf.Mobile.ViewModels
 {
@@ -13,48 +16,108 @@ namespace Xamarin.Bookshelf.Mobile.ViewModels
     {
         private readonly IBookService bookService;
 
+        public ICommand AddToLibraryCommand { get; }
+        public ICommand ReviewBookCommand { get; }
+
         public BookDetailsPageViewModel(IBookService bookService)
         {
             this.bookService = bookService;
+
+            AddToLibraryCommand = new Command(AddToLibrary);
+            ReviewBookCommand = new Command(ReviewBook);
         }
 
-        public string BookId { get; set; }
-
-        private Book book;
-        public Book Book
+        private async void AddToLibrary(object obj)
         {
-            get => book;
-            set => SetProperty(ref book, value);
-    }
-
-        public override async void OnAppearing()
-        {
-            base.OnAppearing();
-
-            await GetBookDetailsAsync();
+            string result = await Shell.Current.DisplayActionSheet("Select a Bookshelf", "Cancel", null, new string[] { "I want to read", "I'm reading", "I already read" });
+            if (!string.IsNullOrWhiteSpace(result) && result != "Cancel")
+            {
+                await RegisterBookAsync(result);
+            }
         }
 
-        public override void OnDisappearing()
-        {
-            Book = null;
-        }
-
-        private async Task GetBookDetailsAsync()
+        private async Task RegisterBookAsync(string selected)
         {
             try
             {
+                ReadingStatus status;
+                if (selected== "I am reading")
+                {
+                    status = ReadingStatus.Reading;
+                }
+                else if (selected == "I already read")
+                {
+                    status = ReadingStatus.Read;
+                }
+                else
+                {
+                    status = ReadingStatus.WantToRead;
+                }
+
                 IsBusy = true;
-                var result = await bookService.GetBookDetailsAsync(BookId).ConfigureAwait(false);
-                Book = result;
+            var bookshelf = new ABookshelf()
+            {
+                BookId = BookId,
+                ReadingStatus = status,
+                UserId = "magoolation@me.com",
+                CreatedAt = DateTimeOffset.Now
+        };
+
+                await bookService.RegisterBookAsync(bookshelf).ConfigureAwait(false);
+                MainThread.BeginInvokeOnMainThread(async () => await Shell.Current.DisplayAlert("Success", "Book added to your Bookshelf successfully!", "OK"));
             }
             catch (Exception ex)
             {
-                await Shell.Current.DisplayAlert("Error", ex.Message, "OK");
-            }
+                MainThread.BeginInvokeOnMainThread(async () => await Shell.Current.DisplayAlert("Error", ex.Message, "OK"));
+}
             finally
             {
                 IsBusy = false;
             }
         }
+
+        private void ReviewBook(object obj)
+{
+    throw new NotImplementedException();
+}
+
+public string BookId { get; set; }
+
+private Book book;
+public Book Book
+{
+    get => book;
+    set => SetProperty(ref book, value);
+}
+
+public override async void OnAppearing()
+{
+    base.OnAppearing();
+
+    await GetBookDetailsAsync();
+}
+
+public override void OnDisappearing()
+{
+    Book = null;
+}
+
+private async Task GetBookDetailsAsync()
+{
+    try
+    {
+        IsBusy = true;
+        var result = await bookService.GetBookDetailsAsync(BookId).ConfigureAwait(false);
+        Book = result;
+    }
+    catch (Exception ex)
+    {
+        await Shell.Current.DisplayAlert("Error", ex.Message, "OK");
+    }
+    finally
+    {
+        IsBusy = false;
+    }
+}
     }
 }
