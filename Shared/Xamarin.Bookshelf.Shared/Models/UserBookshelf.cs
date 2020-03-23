a0940e5c5cb3bae04dd786e6008f37c3b0aa93e0
Alexandre Costa <alexandresantoscosta@gmail.com>
2020-03-23 10:38:58 -0300

    Created a bag for Bookshelves data

---
+++ Shared/Xamarin.Bookshelf.Shared/Models/UserBookshelf.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.Bookshelf.Shared.Models
+{
+    public class UserBookshelf
+    {
+        public string UserId { get; set; }
+        public ReadingStatus ReadingStatus { get; set; }
+        public int Count { get; set; }
+        public Book[] Books { get; set; }
+    }
+}
