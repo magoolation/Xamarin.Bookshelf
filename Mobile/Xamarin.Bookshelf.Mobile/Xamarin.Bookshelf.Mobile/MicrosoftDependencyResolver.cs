a4de98129dc0d061c3e3a476bd5f23055f57c2dd
Alexandre Costa <alexandresantoscosta@gmail.com>
2020-06-20 23:02:23 -0300

    Updated packages and started the implementation of TinyMVVM

---
+++ Mobile/Xamarin.Bookshelf.Mobile/Xamarin.Bookshelf.Mobile/MicrosoftDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TinyMvvm.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace Xamarin.Bookshelf.Mobile
+{
+    public class MicrosoftDependencyResolver : IResolver
+    {
+        private IServiceProvider serviceProvider;
+
+        public MicrosoftDependencyResolver(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public T Resolve<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+
+        public T Resolve<T>(string key)
+        {
+            var services = serviceProvider.GetServices<T>();
+            return services.FirstOrDefault(s => s.GetType().Name == key);
+        }
+
+        public object Resolve(Type type)
+        {
+            return serviceProvider.GetService(type);
+        }
+    }
+}
